ddfe91af694624cf52176e319bb1f2f95fa4d88d
Chinmay Moharir <chinmay.moharir@gmail.com>
2018-01-02 00:15:31 -0800

    Create customer negative scenarios

--- src/test/java/CreateNewCustomer.java
+++ src/test/java/CreateNewCustomer.java
@@ -107,8 +107,9 @@ public class CreateNewCustomer {
 		driver.close();
 
 	}
-	/*
+	
 	@Test
+	@Parameters({ "URL","username", "password" })
 	public void TC_02(String URL, String username, String password) throws Exception{
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Chinmay\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		driver = new ChromeDriver();	
@@ -151,19 +152,15 @@ public class CreateNewCustomer {
 		//check if customer name textbox is present		
 		Assert.assertTrue(driver.findElement(By.name("name")).isDisplayed(), "Customer name text box is not presernt");
 		
-		
-		driver.findElement(By.name("name")).sendKeys("@#@%#");
-		driver.findElement(By.xpath("//p[contains(text(),'Add New Customer')]")).click();
-		Assert.assertTrue(driver.findElement(By.xpath("//label[contains(text(),'Special characters are not allowed')]")).isDisplayed(), "Customer name must not contain special characters message is not appearing on UI");
-		
-		Actions action = new Actions(driver);
-		action.moveToElement(driver.findElement(By.name("name"))).doubleClick().build().perform();
-		
+		//name fiel blank validation
 		driver.findElement(By.name("name")).sendKeys("");
 		driver.findElement(By.xpath("//p[contains(text(),'Add New Customer')]")).click();
 		Assert.assertTrue(driver.findElement(By.xpath("//label[contains(text(),'Customer name must not be blank')]")).isDisplayed(), "Customer name must not be blank message is not appearing on UI");
 		
-		driver.findElement(By.name("name")).sendKeys("Chinmay Moharir");
+		//name field special characters validation
+		driver.findElement(By.name("name")).sendKeys("@#@%#");
+		driver.findElement(By.xpath("//p[contains(text(),'Add New Customer')]")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//label[contains(text(),'Special characters are not allowed')]")).isDisplayed(), "Customer name must not contain special characters message is not appearing on UI");
 		
 		
 		//check if male female radio button is visible
@@ -172,70 +169,79 @@ public class CreateNewCustomer {
 		
 		driver.findElement(By.xpath("//input[@type='radio'][@name='rad1'][@value='m']")).click();
 		
+		//blank date of birth
 		driver.findElement(By.name("dob")).sendKeys("");
 		driver.findElement(By.xpath("//p[contains(text(),'Add New Customer')]")).click();
 		Assert.assertTrue(driver.findElement(By.xpath("//label[contains(text(),'Date Field must not be blank')]")).isDisplayed(), "Date field must not be blank message is not appearing on UI");
 		
-		
-		driver.findElement(By.xpath("//input[@type='date' and @name='dob']")).sendKeys("12");
-		driver.findElement(By.xpath("//input[@type='date' and @name='dob']")).sendKeys("01");
-		driver.findElement(By.xpath("//input[@type='date' and @name='dob']")).sendKeys("1990");
-		
-		
+		//addressfield blank validation
 		Assert.assertTrue(driver.findElement(By.name("addr")).isDisplayed(), "Address text box is not visible");
-		
-		driver.findElement(By.name("addr")).sendKeys("!@$#");
-		driver.findElement(By.xpath("//p[contains(text(),'Add New Customer')]")).click();
-		Assert.assertTrue(driver.findElement(By.xpath("//label[contains(text(),'Special characters are not allowed')]")).isDisplayed(), "Address Field must not have special characters message is not appearing on UI");
-		
 		driver.findElement(By.name("addr")).sendKeys("");
 		driver.findElement(By.xpath("//p[contains(text(),'Add New Customer')]")).click();
 		Assert.assertTrue(driver.findElement(By.xpath("//label[contains(text(),'Address Field must not be blank')]")).isDisplayed(), "Address Field must not be blank message is not appearing on UI");
 		
-		driver.findElement(By.name("addr")).sendKeys("2404 Nutwood Avenue");
+		//address field special characters validation
+		driver.findElement(By.name("addr")).sendKeys("!@$#");
+		driver.findElement(By.xpath("//p[contains(text(),'Add New Customer')]")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//label[contains(text(),'Special characters are not allowed')]")).isDisplayed(), "Address Field must not have special characters message is not appearing on UI");
 		
 		Assert.assertTrue(driver.findElement(By.name("city")).isDisplayed(), "City text box is not visible");
 		
-		driver.findElement(By.name("city")).sendKeys("!@$#");
-		driver.findElement(By.xpath("//p[contains(text(),'Add New Customer')]")).click();
-		
-		Assert.assertTrue(driver.findElement(By.xpath("//label[contains(text(),'Special characters are not allowed')]")).isDisplayed(), "Address Field must not be blank message is not appearing on UI");
-		
+		//city field blank validation
 		driver.findElement(By.name("city")).sendKeys("");
 		driver.findElement(By.xpath("//p[contains(text(),'Add New Customer')]")).click();
 		Assert.assertTrue(driver.findElement(By.xpath("//label[contains(text(),'City Field must not be blank')]")).isDisplayed(), "City field must not be blank message is not appearing on UI");
 		
-		driver.findElement(By.name("city")).sendKeys("Fullerton");
+		//city field special character validation
+		driver.findElement(By.name("city")).sendKeys("!@$#");
+		driver.findElement(By.xpath("//p[contains(text(),'Add New Customer')]")).click();
 		
+		Assert.assertTrue(driver.findElement(By.xpath("//label[contains(text(),'Special characters are not allowed')]")).isDisplayed(), "Address Field must not be blank message is not appearing on UI");
+
 		Assert.assertTrue(driver.findElement(By.name("state")).isDisplayed(), "State text box is not visible");
+		//state field blank validation
 		driver.findElement(By.name("state")).sendKeys("");
 		driver.findElement(By.xpath("//p[contains(text(),'Add New Customer')]")).click();
 		Assert.assertTrue(driver.findElement(By.xpath("//label[contains(text(),'State must not be blank')]")).isDisplayed(), "State field must not be blank message is not appearing on UI");
-		driver.findElement(By.name("state")).sendKeys("California");
-	
+		
+		//state field special character validation
+		driver.findElement(By.name("state")).sendKeys("@##@$");
+		driver.findElement(By.xpath("//p[contains(text(),'Add New Customer')]")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//label[contains(text(),'Special characters are not allowed')]")).isDisplayed(), "State field must not contain special characters message is not appearing on UI");
+
 		Assert.assertTrue(driver.findElement(By.name("pinno")).isDisplayed(), "Postal code text box is not visible");
+		
+		//pin number blank validation
 		driver.findElement(By.name("pinno")).sendKeys("");
 		driver.findElement(By.xpath("//p[contains(text(),'Add New Customer')]")).click();
 		Assert.assertTrue(driver.findElement(By.xpath("//label[contains(text(),'PIN Code must not be blank')]")).isDisplayed(), "PIN Code field must not be blank message is not appearing on UI");
+		
+		//pin number character validation
 		driver.findElement(By.name("pinno")).sendKeys("abcd");
 		Assert.assertTrue(driver.findElement(By.xpath("//label[contains(text(),'Characters are not allowed')]")).isDisplayed(), "PIN Code field characters are not allowed is not appearing on UI");
-		driver.findElement(By.name("pinno")).sendKeys("92831");
+		
+		//pin number special character validation
+		driver.findElement(By.name("pinno")).sendKeys("@#$&$");
+		driver.findElement(By.xpath("//p[contains(text(),'Add New Customer')]")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//label[contains(text(),'Special characters are not allowed')]")).isDisplayed(), "PIN Code field must not contain special characters message is not appearing on UI");
 		
 		Assert.assertTrue(driver.findElement(By.name("telephoneno")).isDisplayed(), "Telephone Number text box is not visible");
+		//telephone number blank validation
 		driver.findElement(By.name("telephoneno")).sendKeys("");
 		driver.findElement(By.xpath("//p[contains(text(),'Add New Customer')]")).click();
 		Assert.assertTrue(driver.findElement(By.xpath("//label[contains(text(),'Mobile no must not be blank')]")).isDisplayed(), "Mobile number field must not be blank message is not appearing on UI");
+		//telephone number character validation
 		driver.findElement(By.name("telephoneno")).sendKeys("abcd");
-		driver.findElement(By.name("telephoneno")).sendKeys("1234567890");
+		Assert.assertTrue(driver.findElement(By.xpath("//label[contains(text(),'Characters are not allowed')]")).isDisplayed(), "PIN Code field characters are not allowed is not appearing on UI");
+		
 		
 		Assert.assertTrue(driver.findElement(By.name("emailid")).isDisplayed(), "Email text box is not visible");
+		//email id validation
 		driver.findElement(By.name("emailid")).sendKeys("asdasdasd");
 		Assert.assertTrue(driver.findElement(By.xpath("//label[contains(text(),'Email-ID is not valid')]")).isDisplayed(), "Invalid email id message is not appearing on UI");
 		
 		
-		
-		
 		driver.close();
 		
-	}*/
+	}
 }
